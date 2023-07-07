5cbf745a97dd35323bce90886e434af9f40ae90d
max <30047099+mxtw@users.noreply.github.com>
2023-07-07 19:09:21 +0200

    raw jq output instead of tr

--- catbg.sh
+++ catbg.sh
@@ -6,7 +6,7 @@ USERAGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, 
 
 # get list of images from json
 # TODO: support more than .jpg
-IMAGES=$(curl -A "$USERAGENT" $URL | jq '.data.children[].data.url | select(. | test(".*.jpg$"))' | tr '"' ' ' )
+IMAGES=$(curl -A "$USERAGENT" $URL | jq -r '.data.children[].data.url | select(. | test(".*.jpg$"))')
 IMG_ARR=($(echo $IMAGES | tr ' ' '\n'))
 
 # pick random image
